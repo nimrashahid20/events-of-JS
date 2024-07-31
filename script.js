0af387359c45dced3f3e7f415d5b68c1e72c9ee4
nimrashahid20 <nimrashahid02007@gmail.com>
2024-07-31 15:45:51 +0500

    Events Of Java Script

---
+++ script.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+// let button = document.getElementById("btn");
+// button.addEventListener("click", () => {
+//   document.querySelector(".container").innerHTML =
+//     "<B>you are clicking the button";
+// });
+
+// button.addEventListener("contextmenu", () => {
+//   document.querySelector(".container").innerHTML =
+//     "<B>you are clicking the button";
+// });
+
+// button.addEventListener("mousedown", () => {
+//     document.querySelector(".container").innerHTML =
+//       "<B>you are clicking the button";
+//   });
+  
+// button.addEventListener("keydown", (e) => {
+//       document.querySelector(".container").innerHTML =
+//        "<B>you are clicking the button";
+//      });
+
+
+// console.log(e.key)
+
+function myfunction(){
+  let y = document.getElementById("fname")
+  y.value = y.value.toUpperCase();
+
+}
+
+function myfunction1(){
+  alert("you passed a key inside the input field")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
